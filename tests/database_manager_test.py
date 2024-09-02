51db2d19c02918f3d8f1335ca1cf0e7cf13e36d8
Marcin <m.stolar2001@gmail.com>
2024-09-02 22:57:32 +0200

    Updating requirements.txt Code formatting

--- tests/database_manager_test.py
+++ tests/database_manager_test.py
@@ -8,49 +8,25 @@ EXAMPLE_PRODUCTS = {
     "products": [
         {
             "name": "egg",
-            "macro": {
-                "fat": 4.8,
-                "protein": 6.3,
-                "carbohydrates": 0.4
-            },
-            "unit": "pieces"
+            "macro": {"fat": 4.8, "protein": 6.3, "carbohydrates": 0.4},
+            "unit": "pieces",
         },
         {
             "name": "wholemeal bread",
-            "macro": {
-                "fat": 1.7,
-                "protein": 5.9,
-                "carbohydrates": 51.2
-            },
-            "unit": "g"
+            "macro": {"fat": 1.7, "protein": 5.9, "carbohydrates": 51.2},
+            "unit": "g",
         },
         {
             "name": "butter",
-            "macro": {
-                "fat": 81,
-                "protein": 0.9,
-                "carbohydrates": 0.1
-            },
-            "unit": "g"
-        }
+            "macro": {"fat": 81, "protein": 0.9, "carbohydrates": 0.1},
+            "unit": "g",
+        },
     ]
 }
 EXAMPLE_RECIPES = {
     "recipes": [
-        {
-            "name": "meal1",
-            "products": {
-                "prod1": 10,
-                "prod2": None
-            }
-        },
-        {
-            "name": "meal2",
-            "products": {
-                "prod3": 25,
-                "prod4": 150
-            }
-        }
+        {"name": "meal1", "products": {"prod1": 10, "prod2": None}},
+        {"name": "meal2", "products": {"prod3": 25, "prod4": 150}},
     ]
 }
 
@@ -60,10 +36,10 @@ EXAMPLE_RECIPES_PATH = Path(__file__).parent.joinpath("test_files", "recipes.jso
 
 
 def create_test_database():
-    with open(EXAMPLE_PRODUCTS_PATH, mode='w') as fp:
+    with open(EXAMPLE_PRODUCTS_PATH, mode="w") as fp:
         json.dump(EXAMPLE_PRODUCTS, fp=fp)
 
-    with open(EXAMPLE_RECIPES_PATH, mode='w') as fp:
+    with open(EXAMPLE_RECIPES_PATH, mode="w") as fp:
         json.dump(EXAMPLE_RECIPES, fp=fp)
 
 
@@ -71,21 +47,11 @@ def test_load_products_should_return_correct_list_of_products():
     products = EXAMPLE_PRODUCTS
 
     expected_output = [
-        Product(name="egg",
-                unit="pieces",
-                fat=4.8,
-                protein=6.3,
-                carbohydrates=0.4),
-        Product(name="wholemeal bread",
-                unit="g",
-                fat=1.7,
-                protein=5.9,
-                carbohydrates=51.2),
-        Product(name="butter",
-                unit="g",
-                fat=81,
-                protein=0.9,
-                carbohydrates=0.1)
+        Product(name="egg", unit="pieces", fat=4.8, protein=6.3, carbohydrates=0.4),
+        Product(
+            name="wholemeal bread", unit="g", fat=1.7, protein=5.9, carbohydrates=51.2
+        ),
+        Product(name="butter", unit="g", fat=81, protein=0.9, carbohydrates=0.1),
     ]
 
     actual_output = load_products(EXAMPLE_PRODUCTS)
