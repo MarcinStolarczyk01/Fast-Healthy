ab12e96deba8034634297d4150185b6fcfe36c72
marcin <m.stolar2001@gmail.com>
2024-10-15 09:38:18 +0200

    Implement FoodDataManager, adjust Product

--- src/food_data_manager/food_data_manager.py
+++ src/food_data_manager/food_data_manager.py
@@ -2,35 +2,39 @@ import os
 import requests
 from exceptions.exceptions import MissingAPIKeyError, MissingNutrientError
 from models.product.product import Product
+from enum import Enum
 
 API_KEY_NAME = 'FD_CENTRAL_API_KEY'
-PROTEIN_ID = 1003
-FAT_ID = 1085
-CARBOHYDRATES_ID = 1005
+
+
+class WantedNutrientIDs(Enum):
+    PROTEIN = 1003
+    FAT = 1085
+    CARBOHYDRATES_ID = 1005
 
 
 class FoodDataManager:
     def __init__(self):
         self.database_api_key = self._get_api_key(API_KEY_NAME)
+        self.api_endpoint = 'https://api.nal.usda.gov/fdc/v1/foods/search'
 
     def products(self, products_names: list[str]) -> tuple[Product, ...]:
-        return [self._search_product(product) for product in products_names]
-
-
-
+        return tuple(self._search_product(name) for name in products_names)
 
     def _search_product(self, name: str, data_type: str = 'Foundation,SR%20Legacy', limit: int = 1) -> Product:
-        request_url = f"https://api.nal.usda.gov/fdc/v1/foods/search?query={name}&dataType={data_type}&pageSize={limit}&api_key={self.database_api_key}"
+        request_url = f"{self.api_endpoint}?query={name}&dataType={data_type}&pageSize={limit}&api_key={self.database_api_key}"
 
         search_results = requests.get(request_url).json()
 
         foods_section = search_results['foods']
-        description = foods_section['description']
-        nutrients_section = foods_section['foodNutrients']
+        name = foods_section['description']
+        nutrients_section: list[dict] = foods_section['foodNutrients']
+        nutrients = {nutrient.name: self._find_nutrient_value(nutrient.value, nutrients_section) for nutrient in
+                     WantedNutrientIDs}
+        return Product(name, nutrients)
 
-
-
-    def _find_nutrient_value(self, nut_id: int, nutrients: list[dict]) -> float:
+    @staticmethod
+    def _find_nutrient_value(nut_id: int, nutrients: list[dict]) -> float:
         for nutrient in nutrients:
             if nutrient["nutrientId"] == nut_id:
                 return nutrient['value']
--- src/models/product/product.py
+++ src/models/product/product.py
@@ -1,44 +1,15 @@
 from dataclasses import dataclass
 
-from typing import Optional
-from exceptions.exceptions import IncompleteProductInfoError
-from keywords.keywords import MacroKeywords
 
 FAT_KCAL = 9
 PROTEIN_KCAL = 4
 CARBOHYDRATES_KCAL = 4
 
 
-@dataclass(frozen=True)
+@dataclass
 class Product:
     name: str
-
-    unit: str
-    fat: float | None = None
-    protein: float | None = None
-    carbohydrates: float | None = None
-
-    def get_kcal(self) -> int:
-        if any(
-            [
-                arg is None
-                for arg in [self.unit, self.fat, self.protein, self.carbohydrates]
-            ]
-        ):
-            raise IncompleteProductInfoError(self)
-        return int(
-            self.fat * FAT_KCAL
-            + self.protein * PROTEIN_KCAL
-            + self.carbohydrates * CARBOHYDRATES_KCAL
-        )
-
-    def get_macro(self) -> dict[str : float | None]:
-        return {
-            MacroKeywords.fat: self.fat,
-            MacroKeywords.protein: self.protein,
-            MacroKeywords.carbohydrates: self.carbohydrates,
-        }
-
-    @staticmethod
-    def incomplete(name: str) -> "Product":
-        return Product(name)
+    nutrients: dict
+    def __post_init__(self):
+        for name in self.nutrients.keys():
+            self.name = self.nutrients[name]
