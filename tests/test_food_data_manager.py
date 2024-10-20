f583eeefd3ad8d3073e12b552dd1cf46d7c5898f
marcin <m.stolar2001@gmail.com>
2024-10-20 20:58:48 +0200

    update the test

--- tests/test_food_data_manager.py
+++ tests/test_food_data_manager.py
@@ -7,18 +7,29 @@ from src.models.product.product import Product
 
 
 # products nutrients data: https://www.nutritionix.com
-@mark.parametrize('requested_product, expected_nutrients', [('cheddar', {'protein': 23,'fat': 33, 'carbohydrates': 3.1}),
-                                                           ('chicken breast', {'protein': 23,'fat': 2.6, 'carbohydrates': 0}),
-                                                           ('egg', {'protein': 13,'fat': 9.5, 'carbohydrates': 0.7})])
-def test_food_data_manager_products_should_return_expected_products(requested_product: str,
-                                                                    expected_nutrients: dict[str: float]) -> None:
+@mark.parametrize(
+    "requested_products, expected_nutrients",
+    [
+        (
+            ["cheddar", "chicken breast raw", "whole egg", "white bread", "rye bread"],
+            [
+                {"protein": 23, "fat": 33, "carbohydrates": 3.1},
+                {"protein": 23, "fat": 2.6, "carbohydrates": 0},
+                {"protein": 13, "fat": 9.5, "carbohydrates": 0.7},
+                {"protein": 9, "fat": 3, "carbohydrates": 49},
+                {"protein": 8.5, "fat": 3.3, "carbohydrates": 48},
+            ],
+        )
+    ],
+)
+def test_food_data_manager_products_should_return_expected_products(
+    requested_products: tuple[str], expected_nutrients: list[dict[str, float]]
+) -> None:
     manager = FoodDataManager()
-    products: tuple[Product, ...] = manager.products(
-        [
-            requested_product,
-        ]
-    )
+    products: tuple[Product, ...] = manager.products(requested_products)
 
-    for product in products:
-        for nutrient, value in expected_nutrients:
-            assert np.isclose(getattr(product, nutrient, None), value, rtol=0.20)
+    for i, product in enumerate(products):
+        for nutrient, value in expected_nutrients[i].items():
+            assert np.isclose(
+                getattr(product, nutrient, None), value, rtol=0.20, atol=3
+            )
