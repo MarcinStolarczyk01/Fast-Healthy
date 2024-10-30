d295230ecb4451e6fe2741c6d055fd4a7dcd237f
marcin <m.stolar2001@gmail.com>
2024-10-30 20:16:52 +0100

    add FilesIOManager unit tests

---
+++ tests/files_io_manager_test.py
@@ -0,0 +1,38 @@
+import json
+from pathlib import Path
+import os
+
+from files_io_manager.files_io_manager import FilesIOManager
+from models.recipe.recipe import RecipeModel
+
+
+def test_add_recipe_should_add_recipe_correctly():
+    recipe_json = RecipeModel(
+        **{
+            "name": "omlet",
+            "procedure": ["Make something", "Make something else", "Finish"],
+            "products": {"egg": 3, "butter": 10},
+        }
+    )
+    recipes_path = Path(__file__).parent.joinpath("test_files", "recipes.json")
+    if recipes_path.exists():
+        os.remove(recipes_path)
+    recipes_path.touch()
+    FilesIOManager.RECIPES_PATH = recipes_path
+
+    FilesIOManager.add_recipe(recipe_json)
+
+    expected_recipes_content = {
+        "recipes": [
+            {
+                "name": "omlet",
+                "procedure": ["Make something", "Make something else", "Finish"],
+                "products": {"egg": 3, "butter": 10},
+            }
+        ]
+    }
+
+    with open(recipes_path) as fp:
+        assert json.loads(fp.read()) == expected_recipes_content
+
+    # todo: What happens when there is no structure in the file? Add empty file creating method to FilesIOManager
