b1ee3386d464285e7bda841f7ae3a0930c24ac71
marcin <m.stolar2001@gmail.com>
2024-11-04 18:04:46 +0100

    dd recipe saving tests

--- tests/files_io_manager_test.py
+++ tests/files_io_manager_test.py
@@ -36,3 +36,44 @@ def test_add_recipe_should_add_recipe_correctly():
         assert json.loads(fp.read()) == expected_recipes_content
 
     # todo: What happens when there is no structure in the file? Add empty file creating method to FilesIOManager
+
+
+def test_add_recipe_should_add_multiple_recipes_correctly():
+    recipe1_json = RecipeModel(
+        **{
+            "name": "omlet",
+            "procedure": ["Make something", "Make something else", "Finish"],
+            "products": {"egg": 3, "butter": 10},
+        }
+    )
+    recipe2_json = RecipeModel(
+        **{
+            "name": "omlet",
+            "procedure": ["Make something", "Make something else", "Finish"],
+            "products": {"egg": 3, "butter": 10},
+        }
+    )
+    recipes_path = Path(__file__).parent.joinpath("test_files", "recipes2.json")
+
+    FilesIOManager.RECIPES_PATH = recipes_path
+
+    FilesIOManager.add_recipe(recipe1_json)
+    FilesIOManager.add_recipe(recipe2_json)
+
+    expected_recipes_content = {
+        "recipes": [
+            {
+                "name": "omlet",
+                "procedure": ["Make something", "Make something else", "Finish"],
+                "products": {"egg": 3, "butter": 10},
+            },
+            {
+                "name": "omlet",
+                "procedure": ["Make something", "Make something else", "Finish"],
+                "products": {"egg": 3, "butter": 10},
+            }
+        ]
+    }
+
+    with open(recipes_path) as fp:
+        assert json.loads(fp.read()) == expected_recipes_content
