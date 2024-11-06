f3b32ac76b1221efa01015251660370001020f5f
marcin <m.stolar2001@gmail.com>
2024-11-06 20:11:52 +0100

    implement api recipes post function

--- src/api.py
+++ src/api.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import ValidationError
+
+from src.dietitian.dietitian import Dietitian
+from src.models.recipe.recipe import RecipeModel
 
 app = FastAPI()
+dietitian = Dietitian()
 
 
 @app.get("/")
@@ -8,12 +13,21 @@ def root():
     return "Welcome to Fast&Healthy API!"
 
 
-@app.get("/recipes")
+@app.get("/recipes", status_code=200)
 def get_recipes():
     raise NotImplemented
 
 
-@app.post("/recipes")
-def post_recipes(recipe):
-    raise NotImplemented
+@app.post("/recipes", status_code=201)
+def post_recipes(recipe: dict):
+    try:
+        dietitian.add_recipe(RecipeModel(**recipe))
+    except ValidationError as e:
+        raise HTTPException(status_code=422, detail=f"ValidationError: {e}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Unexpected Error: {e}")
 
+
+@app.post("/config", status_code=201)
+def configure_dietitian():
+    raise NotImplemented
