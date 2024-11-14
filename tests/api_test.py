33a07a065848c0a77c87fb09ba7f5203cfa73381
marcin <m.stolar2001@gmail.com>
2024-11-14 19:44:15 +0100

    fixing test - still fails

--- tests/api_test.py
+++ tests/api_test.py
@@ -1,4 +1,5 @@
 import json
+import os
 from http import HTTPStatus
 from pathlib import Path
 
@@ -7,6 +8,7 @@ from starlette.testclient import TestClient
 
 from src.api import app
 from src.files_io_manager.files_io_manager import FilesIOManager
+from threading import Lock
 
 client = TestClient(app)
 
@@ -15,35 +17,35 @@ client = TestClient(app)
     "recipe_body, expected_code",
     [
         (
-            {
-                "name": "meal one",
-                "procedure": ["Make something", "Make something else"],
-                "products": {"egg": 3.0, "butter": 10.0},
-            },
-            HTTPStatus.CREATED,
+                {
+                    "name": "meal one",
+                    "procedure": ["Make something", "Make something else"],
+                    "products": {"egg": 3.0, "butter": 10.0},
+                },
+                HTTPStatus.CREATED,
         ),
         (
-            {
-                "name": "meal one",
-                "procedure": "Bad type",
-                "products": {"egg": 3.0, "butter": 10.0},
-            },
-            HTTPStatus.UNPROCESSABLE_ENTITY,
+                {
+                    "name": "meal one",
+                    "procedure": "Bad type",
+                    "products": {"egg": 3.0, "butter": 10.0},
+                },
+                HTTPStatus.UNPROCESSABLE_ENTITY,
         ),
         (
-            {
-                "name": "meal one",
-                "procedure": [
-                    "Brush your teeth!",
-                ],
-                "products": {"egg": 3.0, "butter": 10.0},
-            },
-            HTTPStatus.INTERNAL_SERVER_ERROR,
+                {
+                    "name": "meal one",
+                    "procedure": [
+                        "Brush your teeth!",
+                    ],
+                    "products": {"egg": 3.0, "butter": 10.0},
+                },
+                HTTPStatus.INTERNAL_SERVER_ERROR,
         ),
     ],
 )
 def test_post_recipes_should_return_status_code_correct_code(
-    recipe_body: dict, expected_code: int
+        recipe_body: dict, expected_code: int
 ):
     FilesIOManager.RECIPES_PATH = Path(__file__).parent.joinpath(
         "test_files", "recipes4.json"
@@ -86,13 +88,33 @@ def test_get_recipes_should_return_expected_json(recipes_content):
 
 
 @pytest.mark.parametrize(
-    "recipes_to_del",
-    [{"recipes_to_del": ["*"]}, {"recipes_to_del": ["pancakes", "hamburger"]}],
+    "recipes_to_del, result_recipes_content, fnum",
+    [
+        ({"recipes": ["*"]}, {"recipes": []}, 6),
+        (
+                {"recipes": ["pancakes", "hamburger"]},
+                {
+                    "recipes": [
+                        {
+                            "name": "omlet",
+                            "procedure": [
+                                "Make something",
+                                "Make something else",
+                                "Finish",
+                            ],
+                            "products": {"egg": 3.0, "butter": 10.0},
+                        }
+                    ]
+                },
+        7),
+    ],
 )
-def test_post_recipes_delete_removes_specified_recipes(recipes_to_del):
-    recipes_path = Path(__file__).parent.joinpath("test_files", "recipes6.json")
-    FilesIOManager.RECIPES_PATH = recipes_path
+def test_post_recipes_delete_removes_specified_recipes(
+        recipes_to_del, result_recipes_content, fnum
+):
+    FilesIOManager.RECIPES_PATH = Path(__file__).parent.joinpath('test_files', f'recipes{fnum}.json')
 
+    # Write initial recipes to the file
     recipes_content = {
         "recipes": [
             {
@@ -112,9 +134,14 @@ def test_post_recipes_delete_removes_specified_recipes(recipes_to_del):
             },
         ]
     }
+    with open(FilesIOManager.RECIPES_PATH, "w") as fp:
+        json.dump(recipes_content, fp)
 
-    with open(recipes_path, "w+") as fp:
-        fp.write(json.dumps(recipes_content))
-
+    # Call the API to delete recipes
     response = client.post("/recipes/delete", json=recipes_to_del)
-    assert response.json() == {"message": "Successfully deleted recipes"}
+
+    assert json.loads(response.json()) == {"message": "Successfully deleted recipes"}
+
+    # Read back the content to verify deletion
+    with open(FilesIOManager.RECIPES_PATH, "r") as fp:
+        assert json.load(fp) == result_recipes_content
