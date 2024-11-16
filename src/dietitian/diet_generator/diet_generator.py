d2eceaee818ff7d832f8a4e538244983291b3976
marcin <m.stolar2001@gmail.com>
2024-11-16 20:30:59 +0100

    implement basic generic algorithm

--- src/dietitian/diet_generator/diet_generator.py
+++ src/dietitian/diet_generator/diet_generator.py
@@ -1,30 +1,58 @@
-from typing import Iterable, Iterator
+import logging
 import random
+import time
 
 from src.models.recipe.recipe import Recipe
 
 
 class DietGenerator:
     STAGNATION_LIMIT = 1000
+    TIME_LIMIT = 10
 
     def __init__(self, recipes: tuple[Recipe], kcal_goal: int, meals_number: int):
-        self.recipes = recipes
-        self.kcal_goal = kcal_goal
-        self.meals_number = meals_number
+        self.recipes: tuple[Recipe] = recipes
+        self.kcal_goal: int = kcal_goal
+        self.meals_number: int = meals_number
 
-    def generate(self, days: int = 1) -> tuple[tuple[Recipe, ...]]:
+    def generate(self, days: int = 1) -> tuple[tuple[Recipe, ...], ...]:
         ancestor: list[int] = self._generate_random_chromosome()
         offspring: list[int] = [genome for genome in ancestor]
-        ancestor_kcal: int = sum(recipe.kcal for recipe in self.recipes)
-        while True:
+        ancestor_kcal_gap: int = abs(
+            self.kcal_goal - sum(self.recipes[genome].kcal for genome in ancestor)
+        )
+
+        stagnation = 0
+        start = time.time()
+        while (stagnation < self.STAGNATION_LIMIT) and (
+            time.time() - start < self.TIME_LIMIT
+        ):
             self._mutate(offspring)
-            # offspring_deficit = self.kcal_goal - sum(r)
-            # todo: add calorie counting
+
+            offspring_kcal = sum(self.recipes[genome] for genome in offspring)
+            offspring_kcal_gap = abs(self.kcal_goal - offspring_kcal)
+
+            if offspring_kcal_gap >= ancestor_kcal_gap:
+                stagnation += 1
+            else:
+                ancestor = offspring
+                ancestor_kcal_gap = offspring_kcal_gap
+                stagnation = 0
+
+        logging.info(
+            f"""
+        DietGenerator report
+        --------------------
+        Final kcal_gap:     {ancestor_kcal_gap}
+        Execution time:     {time.time() - start}
+        """
+        )
+
+        return tuple([tuple([self.recipes[genome] for genome in ancestor])])
 
     def _mutate(self, chromosome: list[int]) -> None:
         genome_idx = random.randrange(self.meals_number)
-        genome_value = random.randrange(start=0, stop=len(self.recipes))
-        chromosome[genome_idx] = genome_value
+        genome = random.randrange(start=0, stop=len(self.recipes))
+        chromosome[genome_idx] = genome
 
     def _generate_random_chromosome(self) -> list[int]:
         if len(self.recipes) < 10:
