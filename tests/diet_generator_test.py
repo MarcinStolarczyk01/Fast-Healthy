302a03df795e0f2ef39341e67c991c385980a46d
marcin <m.stolar2001@gmail.com>
2024-11-17 22:13:32 +0100

    test diet generator

---
+++ tests/diet_generator_test.py
@@ -0,0 +1,27 @@
+import json
+import time
+from pathlib import Path
+
+from src.models.recipe.recipe import Recipe, RecipeModel
+from src.files_io_manager.files_io_manager import FilesIOManager
+
+
+def test_placeholder():
+    recipes_json_path = Path(__file__).parent.joinpath('test_files', 'sample_recipes','generated_recipes.json')
+    recipes_content = json.load(open(recipes_json_path))['recipes']
+
+    start = time.time()
+    FilesIOManager.RECIPES_PATH = recipes_json_path.parents[1].joinpath('saved_recipes.json')
+
+    recipes = []
+    for recipe in recipes_content[:15]:
+        recipes.append(Recipe(RecipeModel(**recipe)))
+
+    print(f"\n\nExecution time: {time.time() - start:.4f} seconds")
+
+    from src.dietitian.diet_generator.diet_generator import DietGenerator
+    diet_generator = DietGenerator(recipes=tuple(recipes),
+                                   kcal_goal=3000,
+                                   meals_number=4)
+
+    _ = diet_generator.generate()
